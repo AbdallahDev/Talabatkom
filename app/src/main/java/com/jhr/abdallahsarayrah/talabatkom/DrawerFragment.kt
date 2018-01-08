039c60d7fac9d7b09b0ff9a36de2820e34bd1e1d
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-01-08 14:11:24 +0200

    fix the onCreate override problem, because there was error says onCreate  override nothing

--- app/src/main/java/com/jhr/abdallahsarayrah/talabatkom/DrawerFragment.kt
+++ app/src/main/java/com/jhr/abdallahsarayrah/talabatkom/DrawerFragment.kt
@@ -14,10 +14,9 @@ import android.view.ViewGroup
 class DrawerFragment : Fragment() {
 
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_drawer, container, false)
+        return inflater.inflate(R.layout.fragment_drawer, container, false)
     }
 
 }// Required empty public constructor
